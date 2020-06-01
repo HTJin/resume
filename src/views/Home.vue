a1fd8140d8c634241ebc9502867287220006e6b4
Unknown <htjmario@gmail.com>
2020-06-01 17:32:45 -0500

    pushing up landing page, work in progress

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="profile">
+      <img class="face" alt="Muh face" src="../assets/profile.jpeg" />
+    </div>
+    <div class="link-buttons">
+      <h1>Hyun-Tae Jin</h1>
+      <v-btn outlined color="#75DDDD">See what I can do!</v-btn>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: "Home"
 };
 </script>
+
+<style lang="scss">
+@import "@/styles/home.scss";
+</style>
