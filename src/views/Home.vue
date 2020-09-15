db45a04c5067cfba6d774a1f1849b721a9cf79c2
Unknown <htjmario@gmail.com>
2020-09-15 18:08:41 -0500

    layout redone, added icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,16 +3,30 @@
     <div class="profile">
       <img class="face" alt="Muh face" src="../assets/profile.jpeg" />
     </div>
+    <div class="split"></div>
     <div class="link-buttons">
       <h1>Hyun-Tae Jin</h1>
-      <v-btn outlined color="#75DDDD">See what I can do!</v-btn>
+      <div class="mdiIcons">
+        <v-icon large dark>{{ mdiAt }}</v-icon>
+        <v-icon large dark>{{ mdiGithub }}</v-icon>
+        <v-icon large dark>{{ mdiLinkedin }}</v-icon>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { mdiAt } from "@mdi/js";
+import { mdiGithub } from "@mdi/js";
+import { mdiLinkedin } from "@mdi/js";
+
 export default {
-  name: "Home"
+  name: "Home",
+  data: () => ({
+    mdiGithub: mdiGithub,
+    mdiAt: mdiAt,
+    mdiLinkedin: mdiLinkedin
+  })
 };
 </script>
 
