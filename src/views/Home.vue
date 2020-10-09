48000a669a6dac0cf0bad8ea02b7729f43388381
Unknown <htjmario@gmail.com>
2020-10-09 17:19:13 -0400

    added to router and animation, experimenting with parallax

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,92 +1,103 @@
 <template>
-  <div class="home">
-    <div class="mainbody">
-      <div class="profile">
-        <img class="face" alt="Muh face" src="../assets/profile.jpeg" />
-      </div>
-      <div class="split"></div>
-      <div class="link-buttons">
-        <h1>Hyun-Tae Jin</h1>
-        <div class="mdiIcons">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="mailto:htjmario@gmail.com"
-          >
-            <v-tooltip bottom color="#172A3A">
-              <template v-slot:activator="{ on }">
-                <v-hover v-slot:default="{ hover }">
-                  <v-icon
-                    large
-                    dark
-                    v-on="on"
-                    :color="hover ? '#CE493B' : '#FFF'"
-                    >{{ mdiGmail }}</v-icon
-                  >
-                </v-hover>
-              </template>
-              <span>htjmario@gmail.com</span>
-            </v-tooltip>
-          </a>
-          <v-icon small dark>{{ mdiPowerOn }}</v-icon>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://github.com/HTJin"
-          >
-            <v-tooltip bottom color="#172A3A">
-              <template v-slot:activator="{ on }">
-                <v-hover v-slot:default="{ hover }">
-                  <v-icon
-                    large
-                    dark
-                    v-on="on"
-                    :color="hover ? '#A66CFF' : '#FFF'"
-                    >{{ mdiGithub }}</v-icon
-                  >
-                </v-hover>
-              </template>
-              <span>github.com/htjin</span>
-            </v-tooltip>
-          </a>
-          <v-icon small dark>{{ mdiPowerOn }}</v-icon>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://linkedin.com/in/HTJin"
-          >
-            <v-tooltip bottom color="#172A3A">
-              <template v-slot:activator="{ on }">
-                <v-hover v-slot:default="{ hover }">
-                  <v-icon
-                    large
-                    dark
-                    v-on="on"
-                    :color="hover ? '#0077B5' : '#FFF'"
-                    >{{ mdiLinkedin }}</v-icon
-                  >
-                </v-hover>
-              </template>
-              <span>linkedin/in/HTJin</span>
-            </v-tooltip>
-          </a>
+  <transition name="slide-fade">
+    <div v-if="show" class="home">
+      <div class="mainbody">
+        <div class="profile">
+          <img class="face" alt="Muh face" src="../assets/profile.jpeg" />
+        </div>
+        <div class="split"></div>
+        <div class="link-buttons">
+          <h1>Hyun-Tae Jin</h1>
+          <div class="mdiIcons">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="mailto:htjmario@gmail.com"
+            >
+              <v-tooltip bottom color="#172A3A">
+                <template v-slot:activator="{ on }">
+                  <v-hover v-slot:default="{ hover }">
+                    <v-icon
+                      large
+                      dark
+                      v-on="on"
+                      :color="hover ? '#CE493B' : '#FFF'"
+                      >{{ mdiGmail }}</v-icon
+                    >
+                  </v-hover>
+                </template>
+                <span>htjmario@gmail.com</span>
+              </v-tooltip>
+            </a>
+            <v-icon small dark>{{ mdiPowerOn }}</v-icon>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/HTJin"
+            >
+              <v-tooltip bottom color="#172A3A">
+                <template v-slot:activator="{ on }">
+                  <v-hover v-slot:default="{ hover }">
+                    <v-icon
+                      large
+                      dark
+                      v-on="on"
+                      :color="hover ? '#A66CFF' : '#FFF'"
+                      >{{ mdiGithub }}</v-icon
+                    >
+                  </v-hover>
+                </template>
+                <span>github.com/htjin</span>
+              </v-tooltip>
+            </a>
+            <v-icon small dark>{{ mdiPowerOn }}</v-icon>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://linkedin.com/in/HTJin"
+            >
+              <v-tooltip bottom color="#172A3A">
+                <template v-slot:activator="{ on }">
+                  <v-hover v-slot:default="{ hover }">
+                    <v-icon
+                      large
+                      dark
+                      v-on="on"
+                      :color="hover ? '#0077B5' : '#FFF'"
+                      >{{ mdiLinkedin }}</v-icon
+                    >
+                  </v-hover>
+                </template>
+                <span>linkedin/in/HTJin</span>
+              </v-tooltip>
+            </a>
+          </div>
         </div>
       </div>
+      <v-tooltip top color="#09BC8A">
+        <template v-slot:activator="{ on }">
+          <div class="down" v-on="on">
+            <router-link to="/timeline">
+              <v-hover v-slot:default="{ hover }">
+                <v-icon
+                  v-if="hover"
+                  large
+                  dark
+                  color="#09BC8A"
+                  @click="show = !show"
+                  >{{ mdiChevronTripleDown }}</v-icon
+                >
+                <v-icon v-else large dark color="#FFF">{{
+                  mdiChevronDown
+                }}</v-icon>
+              </v-hover>
+            </router-link>
+          </div>
+        </template>
+        <span>See what I can do!</span>
+      </v-tooltip>
     </div>
-    <v-tooltip top color="#09BC8A">
-      <template v-slot:activator="{ on }">
-        <div class="down" v-on="on">
-          <v-hover v-slot:default="{ hover }">
-            <v-icon v-if="hover" large dark color="#09BC8A">{{
-              mdiChevronTripleDown
-            }}</v-icon>
-            <v-icon v-else large dark color="#FFF">{{ mdiChevronDown }}</v-icon>
-          </v-hover>
-        </div>
-      </template>
-      <span>See what I can do!</span>
-    </v-tooltip>
-  </div>
+  </transition>
 </template>
 
 <script>
@@ -105,7 +116,8 @@ export default {
     mdiLinkedin: mdiLinkedin,
     mdiPowerOn: mdiPowerOn,
     mdiChevronDown: mdiChevronDown,
-    mdiChevronTripleDown: mdiChevronTripleDown
+    mdiChevronTripleDown: mdiChevronTripleDown,
+    show: true
   })
 };
 </script>
